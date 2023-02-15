ffbeec6bc2212d72205ce467ba31337bc923e1a1
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2023-02-15 08:13:22 +0700

    Add files via upload

---
+++ TugasAcara7.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas.acara.pkg7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer E5
+ */
+public class TugasAcara7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("Nama saya Nisa");
+        
+        System.out.println("Hello World!");
+        System.out.println("Nama saya Nisa");
+        
+        int panjang = 10;
+        System.out.println(panjang);
+        
+        int lebar = 10;
+        System.out.println("lebar segi empat: " + lebar);
+        
+        Scanner sc = new Scanner(System.in);
+        int usia;
+        System.out.print("Masukkan usia anda: ");
+        usia = sc.nextInt();
+        System.out.print("Usia anda adalah: "+ usia);
+        
+        Scanner sa = new Scanner(System.in);
+        int tinggi, berat;
+        System.out.print("Massukkan tinggi: ");
+        tinggi = sa.nextInt();
+        System.out.print("Masukkan berat: ");
+        berat = sa.nextInt();
+        System.out.print("Nilai dari tinggi: " + tinggi);
+        System.out.print("Nilai dari berat: " + berat);
+        
+         
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println (nama);
+        System.out.println(a);
+        System.out.println(abjad);
+        System.out.println(b);
+                
+    } 
+        
+        
+    }
+                
+        
+       
+        
+    
+  
+   
+    
+    
+
